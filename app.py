3b5e1c490702fe9c89b1f868735a438104bc4c89
Afeefa Batool <afeefabatoolpgc@gmail.com>
2024-08-31 17:32:30 +0500

    update app.py

--- app.py
+++ app.py
@@ -23,9 +23,3 @@ st.dataframe(data)
 st.write("Plotting a simple line chart:")
 st.line_chart(data)
 
-
-st.title("Hello, Streamlit!")
-st.write("This is a simple Streamlit app.")
-
-number = st.slider("Pick a number", 0, 100)
-st.write(f"The square of {number} is {number ** 2}.")
